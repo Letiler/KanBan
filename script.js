260924822dfd58dcf4d751c184375adf832e1eee
Micah <micah2021190@live.ytech.edu>
2019-11-06 10:43:15 -0500

    have fun

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ function more() {
   var node = document.createElement("LI");
   var textnode =
       document.createTextNode(a);
+  var img = document.createElement('img'); 
+  img.src = "X.png"
+  img.className = "cancelimg"
+  img.onclick = function remove() {
+    
+  }
   node.className = "ui-state-default double"
   node.appendChild(textnode);
+  node.appendChild(img);
   document.getElementById("sortable1").appendChild(node).classname;
 }
 
 // this is the remove button
 function remove() {
-  var doc3 = document.getElementById("sortable3")
-  doc3.removeChild(doc3.childNodes[0]);
+  
 }
 
 // this is a jquary to make the
@@ -21,10 +27,4 @@ function remove() {
      connectWith: ".connectedSortable"
    }).disableSelection();
   } );
-
-  function add() {
-    var para = document.createElement("P");
-    para.innerText = document.getElementById("card").value;
-    document.body.appendChild(para);
-  }
 
